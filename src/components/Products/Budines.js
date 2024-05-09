91e74775c22a80646bd40ea952d4ab7412c7917a
Nazareno Gutierrez <nazarenojunin@gmail.com>
2024-05-08 23:55:52 -0300

    Added new products

--- src/components/Products/Budines.js
+++ src/components/Products/Budines.js
@@ -1,16 +1,24 @@
-import budin1 from '../../assets/budines/budin1.jpg';
-
-import budin2 from '../../assets/budines/budin-prueba2.jpg';
-import budin3 from '../../assets/budines/budin-prueba3.avif';
-import budinChoco1 from '../../assets/budines/budin-choco.jpg';
-import budinChoco2 from '../../assets/budines/budin-choco2.jpg';
-import budinChoco3 from '../../assets/budines/budin-choco3.jpg';
-import budinBanana1 from '../../assets/budines/budin-banana.avif';
-import budinBanana2 from '../../assets/budines/budin-banana2.avif';
-import budinBanana3 from '../../assets/budines/budin-banana3.avif';
-import budinLimon1 from '../../assets/budines/budin-limon.jpg';
-import budinLimon2 from '../../assets/budines/budin-limon2.jpg';
-import budinLimon3 from '../../assets/budines/budin-limon3.webp';
+import budin1 from '../../assets/budines/budin1.avif';
+import budin2 from '../../assets/budines/budin2.avif';
+import budin3 from '../../assets/budines/budin3.avif';
+import budinChoco1 from '../../assets/budines/budin-choco1.avif';
+import budinChoco2 from '../../assets/budines/budin-choco2.avif';
+import budinChoco3 from '../../assets/budines/budin-choco3.avif';
+import budinLimon1 from '../../assets/budines/budin-limon1.avif';
+import budinLimon2 from '../../assets/budines/budin-limon2.avif';
+import budinLimon3 from '../../assets/budines/budin-limon3.avif';
+import budinMarmolado1 from '../../assets/budines/budin-marmolado1.avif';
+import budinMarmolado2 from '../../assets/budines/budin-marmolado2.avif';
+import budinMarmolado3 from '../../assets/budines/budin-marmolado3.avif';
+import budinFrutos1 from '../../assets/budines/budin-frutos1.avif';
+import budinFrutos2 from '../../assets/budines/budin-frutos2.avif';
+import budinFrutos3 from '../../assets/budines/budin-frutos3.avif';
+import budinPan1 from '../../assets/budines/budin-pan1.avif';
+import budinPan2 from '../../assets/budines/budin-pan2.avif';
+import budinPan3 from '../../assets/budines/budin-pan3.avif';
+import budinNaranja1 from '../../assets/budines/budin-naranja1.avif';
+import budinNaranja2 from '../../assets/budines/budin-naranja2.avif';
+import budinNaranja3 from '../../assets/budines/budin-naranja3.avif';
 
 const Budines = [
   {
@@ -19,7 +27,7 @@ const Budines = [
     description: "Budín clásico sabor vainilla",
     img: budin1,
     imgArray: [budin1, budin2, budin3],
-    price: "200 c/u"
+    price: "1500 c/u"
   },
   {
     id: 2,
@@ -27,7 +35,7 @@ const Budines = [
     description: "Budín con cobertura y sabor a limón",
     img: budinLimon1,
     imgArray: [budinLimon1, budinLimon2, budinLimon3],
-    price: "250 c/u"
+    price: "2000 c/u"
   },
   {
     id: 3,
@@ -35,15 +43,39 @@ const Budines = [
     description: "Budín de chocolate con trozos de chocolate grandes",
     img: budinChoco1,
     imgArray: [budinChoco1, budinChoco2, budinChoco3],
-    price: "300 c/u"
+    price: "2300 c/u"
   },
   {
     id: 4,
-    title: "Budín de banana",
-    description: "Budín con sabor banana y trocitos de banana",
-    img: budinBanana3,
-    imgArray: [budinBanana3, budinBanana2, budinBanana1],
-    price: "150 c/u"
+    title: "Budín de pan",
+    description: "Budín de pan con caramelo encima",
+    img: budinPan3,
+    imgArray: [budinPan3, budinPan2, budinPan1],
+    price: "2200 c/u"
+  },
+  {
+    id: 5,
+    title: "Budín marmolado",
+    description: "Budín de vainilla marmolado con chocolate",
+    img: budinMarmolado1,
+    imgArray: [budinMarmolado1, budinMarmolado2, budinMarmolado3],
+    price: "2000 c/u"
+  },
+  {
+    id: 6,
+    title: "Budín de naranja",
+    description: "Budín de naranja",
+    img: budinNaranja1,
+    imgArray: [budinNaranja1, budinNaranja2, budinNaranja3],
+    price: "2000 c/u"
+  },
+  {
+    id: 7,
+    title: "Budín de frutos secos",
+    description: "Budín con frutos secos trozeados",
+    img: budinFrutos2,
+    imgArray: [budinFrutos2, budinFrutos3, budinFrutos1],
+    price: "2600 c/u"
   },
 ];
 
--- src/components/Products/Cookies.js
+++ src/components/Products/Cookies.js
@@ -1,24 +1,33 @@
-import cookieImg from '../../assets/cookies/cookie-sin-fondo.png';
-import cookie2Img from '../../assets/cookies/cookie-prueba1.jpg';
-import cookie3Img from '../../assets/cookies/cookie-prueba3.jpg';
-
-import cookieRedVelvet1 from '../../assets/cookies/cookie-red-velvet.jpg';
-import cookieRedVelvet2 from '../../assets/cookies/cookie-red-velvet2.webp';
-import cookieRedVelvet3 from '../../assets/cookies/cookie-red-velvet3.webp';
-import cookieNueces1 from '../../assets/cookies/cookie-nueces.jpg';
-import cookieNueces2 from '../../assets/cookies/cookie-nueces2.jpg';
-import cookieBrownie1 from '../../assets/cookies/cookie-brownie.jpg';
-import cookieBrownie2 from '../../assets/cookies/cookie-brownie2.png';
-import cookieBrownie3 from '../../assets/cookies/cookie-brownie3.jpg';
+import cookie1Img from '../../assets/cookies/cookie-sin-fondo.avif';
+import cookie2Img from '../../assets/cookies/cookie2.avif';
+import cookie3Img from '../../assets/cookies/cookie3.avif';
+import cookieRedVelvet1 from '../../assets/cookies/cookie-red1.avif';
+import cookieRedVelvet2 from '../../assets/cookies/cookie-red2.avif';
+import cookieRedVelvet3 from '../../assets/cookies/cookie-red3.avif';
+import cookieNueces1 from '../../assets/cookies/cookie-nueces1.avif';
+import cookieNueces2 from '../../assets/cookies/cookie-nueces2.avif';
+import cookieNueces3 from '../../assets/cookies/cookie-nueces3.avif';
+import cookieBrownie1 from '../../assets/cookies/cookie-brownie1.avif';
+import cookieBrownie2 from '../../assets/cookies/cookie-brownie2.avif';
+import cookieBrownie3 from '../../assets/cookies/cookie-brownie3.avif';
+import cookieColor1 from '../../assets/cookies/cookie-color1.avif';
+import cookieColor2 from '../../assets/cookies/cookie-color2.avif';
+import cookieColor3 from '../../assets/cookies/cookie-color3.avif';
+import cookieNavideña1 from '../../assets/cookies/cookie-navidad1.avif';
+import cookieNavideña2 from '../../assets/cookies/cookie-navidad2.avif';
+import cookieNavideña3 from '../../assets/cookies/cookie-navidad3.avif';
+import cookieDdl1 from '../../assets/cookies/cookie-ddl1.avif';
+import cookieDdl2 from '../../assets/cookies/cookie-ddl2.avif';
+import cookieDdl3 from '../../assets/cookies/cookie-ddl3.avif';
 
 const Cookies = [
   {
     id: 1,
     title: "Cookie clásica",
     description: "Cookie clásica con trozos grandes de chocolate y algunas cosas como las otras que tiene el budin",
-    img: cookieImg,
-    imgArray: [cookieImg, cookie2Img, cookie3Img],
-    price: "200 c/u"
+    img: cookie1Img,
+    imgArray: [cookie1Img, cookie2Img, cookie3Img],
+    price: "600 c/u"
   },
   {
     id: 2,
@@ -26,7 +35,7 @@ const Cookies = [
     description: "Cookie tipo red velvet con trozos grandes de chocolate",
     img: cookieRedVelvet1,
     imgArray: [cookieRedVelvet1, cookieRedVelvet2, cookieRedVelvet3],
-    price: "250 c/u"
+    price: "850 c/u"
   },
   {
     id: 3,
@@ -34,15 +43,39 @@ const Cookies = [
     description: "Cookie de browine con centro de dulce de leche",
     img: cookieBrownie2,
     imgArray: [cookieBrownie2, cookieBrownie3, cookieBrownie1],
-    price: "300 c/u"
+    price: "850 c/u"
   },
   {
     id: 4,
     title: "Cookie con nueces",
     description: "Cookie con nueces y frutos secos",
-    img: cookieNueces2,
-    imgArray: [cookieNueces2, cookieNueces1],
-    price: "150 c/u"
+    img: cookieNueces3,
+    imgArray: [cookieNueces3, cookieNueces1, cookieNueces2],
+    price: "700 c/u"
+  },
+  {
+    id: 5,
+    title: "Cookie de colores",
+    description: "Cookie con de colores y sabores con rocklets / chips de chocolate",
+    img: cookieColor1,
+    imgArray: [cookieColor1, cookieColor2, cookieColor3],
+    price: "700 c/u"
+  },
+  {
+    id: 6,
+    title: "Cookie navideña",
+    description: "Cookie con formas de arbol navideño y figuras",
+    img: cookieNavideña2,
+    imgArray: [cookieNavideña2, cookieNavideña1, cookieNavideña3],
+    price: "950 c/u"
+  },
+  {
+    id: 7,
+    title: "Cookie con ddl",
+    description: "Cookie rellena de dulce de leche",
+    img: cookieDdl1,
+    imgArray: [cookieDdl1, cookieDdl2, cookieDdl3],
+    price: "850 c/u"
   },
 ];
 
