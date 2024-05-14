9fc0154c9976c9ccf378d7492219481c94ad1064
Nazareno Gutierrez <nazarenojunin@gmail.com>
2024-05-13 21:52:47 -0300

    Removed framer motion from header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import logoCookta from "../../assets/logo-cookta.webp";
+import logoCookta from "../../assets/logo-cookta.png";
 import { Link } from "react-router-dom";
-import { motion } from "framer-motion";
 import { SiInstagram } from "react-icons/si";
 import { BsWhatsapp } from "react-icons/bs";
 import Footer from "../Footer/Footer";
@@ -10,12 +9,7 @@ import SliderMenu from "../Slider/SliderMenu";
 const Header = ({ children }) => {
   return (
     <>
-      <motion.div
-        className="flex fixed w-full h-20 items-center bg-brown font-medium justify-between p-4 shadow-md z-30"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 2.8, ease: "easeIn" }}
-      >
+      <div className="flex fixed w-full h-20 items-center bg-brown-cookie font-medium justify-between p-4 shadow-md z-30">
         <Link to="/">
           <img
             src={logoCookta}
@@ -24,57 +18,32 @@ const Header = ({ children }) => {
           />
         </Link>
         <div className="gap-10 hidden sm:flex">
-          <div className="transition-all">
-            <Link to="/" className="nav-item">
-              Inicio
-            </Link>
-          </div>
-          <div className="transition-all">
-            <Link to="/nosotros" className="nav-item">
-              Nosotros
-            </Link>
-          </div>
-          <div className="transition-all">
-            <Link to="/como-comprar" className="nav-item">
-              Como Comprar
-            </Link>
-          </div>
+          <Link to="/" className="nav-item">Inicio</Link>
+          <Link to="/nosotros" className="nav-item">Nosotros</Link>
+          <Link to="/como-comprar" className="nav-item">Como Comprar</Link>
         </div>
         <div className="flex gap-3 text-brown-dark justify-end">
-          <motion.a
-            className="text-3xl cursor-pointer hidden sm:block"
-            whileHover={{ scale: 1.15, rotate: 2 }}
-            whileTap={{
-              scale: 0.98,
-              rotate: 0,
-            }}
+          <a
+            className="text-3xl cursor-pointer hidden sm:block hover:scale-105 transition-all"
             href="https://www.instagram.com/cookta__/"
             target="_blank"
+            rel="noreferrer"
           >
             <SiInstagram />
-          </motion.a>
-          <motion.a
-            className="text-3xl cursor-pointer hidden sm:block"
-            whileHover={{ scale: 1.15, rotate: 2 }}
-            whileTap={{
-              scale: 0.98,
-              rotate: 0,
-            }}
+          </a>
+          <a
+            className="text-3xl cursor-pointer hidden sm:block hover:scale-105 transition-all"
             href="https://wa.me/2364673632/?text=Hola!%20Me%20interesa%20un%20producto"
             target="_blank"
+            rel="noreferrer"
           >
             <BsWhatsapp />
-          </motion.a>
-          <motion.a
-            className="block sm:hidden"
-            whileTap={{
-              scale: 0.70,
-            }}
-          >
+          </a>
+          <button className="block sm:hidden">
             <SliderMenu />
-          </motion.a>
+          </button>
         </div>
-      </motion.div>
+      </div>
       <div className="h-full w-full">{children}</div>
       <Footer />
     </>
