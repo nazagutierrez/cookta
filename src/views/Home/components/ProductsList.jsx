534e4f9c77f96d7a4af8e52253a2e43460e1ecf2
Nazareno Gutierrez <nazarenojunin@gmail.com>
2024-05-13 21:55:45 -0300

    Removed framer motion and restyled the section

--- src/views/Home/components/ProductsList.jsx
+++ src/views/Home/components/ProductsList.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import CooktaModal from "./CooktaModal";
-import { motion } from "framer-motion";
+import styles from "./product-shadow.module.css"
 
 function descriptionShorter(desc) {
   if (desc.length > 70) {
@@ -13,32 +13,27 @@ function descriptionShorter(desc) {
 const ProductsList = ({ products, title, textColor, bgColor }) => {
   const [product] = useState(products);
   return (
-    <div className="flex flex-col items-center justify-center gap-5 mb-16 h-full">
-      <h1 className="text-brown-dark-2 mb-5 title-style">{title}</h1>
+    <section className="flex flex-col items-center justify-center gap-5 mb-16 h-full">
+      <h1 style={{ boxShadow: "0px 0 15px 20px #C39A6C"}} className="text-brown-dark-2 bg-brown mt-5 mb-10 title-style">{title}</h1>
       <div className="flex justify-center items-center gap-5 flex-wrap">
         {product.map((product) => {
           const { id, img, title, description, price, imgArray} = product;
           return (
-          <motion.div
-            whileHover={{ scale: 1.05, rotate: 2 }}
-            whileTap={{
-              scale: 0.95,
-              rotate: 0,
-            }}
+          <div
             key={id}
-            className={`${bgColor} flex flex-col justify-center items-center border border-brown shadow-brown shadow-lg rounded-md w-80 sm:w-1/2 max-w-xl cursor-pointer`}
+            className={`${bgColor} ${styles.box} rounded-md hover:scale-105 hover:rotate-2 transition cursor-pointer`}
           >
             <CooktaModal img={img} title={title} description={description} price={price} imgArray={imgArray} textColor={textColor} bgColor={bgColor}>
-              <img src={img} alt="cookie-img" className="h-96 w-80 my-3 object-cover" />
+              <img src={img} alt="cookie-img" className="h-80 w-72 my-3 object-cover" />
               <h1 className={`${textColor} text-3xl`}>{title}</h1>
               <p className={`${textColor} text-xl text-center mb-1 w-80 md:w-96 px-2 h-20 md:h-14`}>{descriptionShorter(description)}</p>
               <p className={`${textColor} text-xl mb-1`}>{price}</p>
             </CooktaModal>
-          </motion.div>
+          </div>
           )
         })}
       </div>
-    </div>
+    </section>
   );
 };
 
