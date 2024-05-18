d9fe3eb82ed1913b8cdc9a652bf1151a67a47b9b
Nazareno Gutierrez <nazarenojunin@gmail.com>
2024-05-17 21:27:43 -0300

    Added responsive to Init animation

--- src/views/Home/components/InitAnimation.jsx
+++ src/views/Home/components/InitAnimation.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const InitAnimation = () => {
   const animationStyle =
-    "z-40 pointer-events-none fixed text-9xl home-bg border-brown-dark content-center bg-brown h-full w-full text-brown-dark font-light";
+    "z-40 pointer-events-none fixed text-7xl md:text-8xl lg:text-9xl home-bg border-brown-dark content-center bg-brown h-full w-full text-brown-dark font-light";
   return (
     <>
       <div className={`${animationStyle} left-0 border-r-2 text-end animation-left`}>
-        <h1 className="bg-[#C29A6A] blur-l-box float-right w-60 h-48 content-center">Coo</h1>
+        <h1 className="bg-[#C29A6A] blur-l-box float-right lg:w-60 md:w-48 h-36 w-36 content-center">Coo</h1>
       </div>
       <div className={`${animationStyle} right-0 border-l-2 animation-right`}>
-        <h1 className="bg-[#C29A6A] blur-r-box w-44 h-48 content-center">kta</h1>
+        <h1 className="bg-[#C29A6A] blur-r-box lg:w-44 w-36 md:w-48 h-36 content-center">kta</h1>
       </div>
     </>
   );
